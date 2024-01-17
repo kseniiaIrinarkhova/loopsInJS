743907511a7862b4a6cf085f95a5254eb3b3c353
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-01-17 14:26:33 -0800

    part 2: prime time explanation

--- index.js
+++ index.js
@@ -30,3 +30,18 @@ for (let i = 0; i <= 100; i++) {
     };
     
 }
+
+/**
+ * Part 2: Prime Time
+Write a script that accomplishes the following:
+    Declare an arbitrary number, n.
+    Create a loop that searches for the next prime number, starting at n and incrementing from there.
+    As soon as you find the prime number, log that number and exit the loop.
+Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+ */
+
+console.log(`
+************************
+***Part 2: Prime Time***
+************************
+`);
